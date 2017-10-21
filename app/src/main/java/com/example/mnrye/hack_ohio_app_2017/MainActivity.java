b8c621c6f606c0134193c85b62935edec2809ce7
Twin802 <tominthehat@gmail.com>
2017-10-21 14:43:14 -0400

    Added Map

--- app/src/main/java/com/example/mnrye/hack_ohio_app_2017/MainActivity.java
+++ app/src/main/java/com/example/mnrye/hack_ohio_app_2017/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +25,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+        final Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.activity_maps);
             }
         });
     }
